8362a418460c793cf4205c86d142eefc1513893c
Yordan <yordan_yordanov001@abv.bg>
2021-12-23 18:39:50 +0200

    solution

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,7 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-
+  const spanPrice = document.querySelector(".price");
+  const priceValue = spanPrice.innerText;
+  spanPrice.setAttribute("data-price", priceValue);
 });
