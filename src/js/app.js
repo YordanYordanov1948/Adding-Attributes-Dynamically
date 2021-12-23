1f8deaa61a304a184e01fc7eefdafac76e785fbe
Yordan <yordan_yordanov001@abv.bg>
2021-12-23 19:17:14 +0200

    solution

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,4 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
-  const spanPrice = document.querySelector(".price");
-  const priceValue = spanPrice.innerText;
-  spanPrice.setAttribute("data-price", priceValue);
 });
