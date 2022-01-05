c757445fe77256a7ee91885aad42b46ba5260b28
Yordan <yordan_yordanov001@abv.bg>
2022-01-05 18:41:43 +0200

    solution

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,4 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
-  const products = Array.from(document.querySelectorAll(".product"));
-  products.forEach(function (product) {
-    product.setAttribute(
-      "data-price",
-      product.querySelectorAll(".price").innerText
-    );
-  });
 });
